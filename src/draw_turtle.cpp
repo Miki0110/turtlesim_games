99a2979578a2c08d131c02397059b4ad7d93c663
Miki Pregaard <mikipregaard@yahoo.dk>
2020-10-22 19:56:28 +0200

    doesn't work

--- src/draw_turtle.cpp
+++ src/draw_turtle.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "stdlib.h"
 #include "time.h"
+bool temp = 0;
 
 int randomNumber(int maxN, int minN){
     srand (time(NULL));
@@ -36,16 +37,17 @@ bool itIntersects(int max_angle,int x1, int y1, int x2, int y2){
         double x_distance = x1 - x2;
         double y_distance = y1 - y2;
         double header = atan2(y_distance, x_distance);
-        double angular_difference = header * 180 / M_PI;
-  ROS_INFO("\nAngle: %f", angular_difference);
-  if (angular_difference < max_angle && angular_difference > -max_angle)
-  {
-    return false;
-  }
-  else
+        double angular_difference = header - temp;
+        header = temp;
+  if (angular_difference > M_PI && angular_difference < (-M_PI))
+    {
+        return false;
+    }
+else
   {
     return true;
   }
+  ROS_INFO("\nAngle: %f", angular_difference);
 }
 void swapValue(int val1, int val2){
     int temp = val1;
@@ -78,53 +80,43 @@ int main(int argc, char *argv[]) {
   int coordinatesX[sizeCoordA+1];
   int coordinatesY[sizeCoordA+1]; 
 
-for(int i=0; i<sizeCoordA+1; i++){
+for(int i=0; i<sizeCoordA+3; i++){
   coordinatesX[i] = randomNumber(10, 3);
   ros::Duration(1.0).sleep();
   coordinatesY[i] = randomNumber(10, 3);
   ros::Duration(1.0).sleep();
     }
-    bool fullIntersect = true;
-while (fullIntersect == true)
+    bool sorted = false;
+while (!sorted)
 {
 
 for(int i=0; i<sizeCoordA+1; i++){
   
-  int intersection = itIntersects(130, coordinatesX[i], coordinatesY[i], coordinatesX[i-1], coordinatesY[i-1]);
+  int intersection = itIntersects(130, coordinatesX[i], coordinatesY[i], coordinatesX[i+1], coordinatesY[i+1]);
   
   if (intersection = true){
-    swapValue(coordinatesX[i], coordinatesX[i-1]);
-    swapValue(coordinatesY[i], coordinatesY[i-1]);
+    swapValue(coordinatesX[i+1], coordinatesX[i]);
+    swapValue(coordinatesY[i+1], coordinatesY[i]);
     ROS_INFO("\nsortting");
   }
     }
 
-fullIntersect = false;
+sorted = true;
 for(int i=0; i<sizeCoordA; i++){
   //std::cout << "(" << coordinatesX[i] << ", " << coordinatesY[i] << ") ";
   int intersection = itIntersects(130, coordinatesX[i], coordinatesY[i], coordinatesX[i+1], coordinatesY[i+1]);
   
   if (intersection = true){
-    fullIntersect = true;
+     sorted = false;
     ROS_INFO("\nfailed sort");
   }
 }
 }
-/*std::sort(coordinatesX, coordinatesX + sizeCoordA);
-std::sort(coordinatesY, coordinatesY + sizeCoordA);*/
-  for(int i=0; i<sizeCoordA+1; i++){
+
+for(int i=0; i<sizeCoordA+1; i++){
 std::cout << "(" << coordinatesX[i] << ", " << coordinatesY[i] << ") ";
   }
 
-//for calculating the angle
-for(int i=0; i<sizeCoordA+1; i++){
-double x_distance = coordinatesX[i] - coordinatesX[i-1];
-double y_distance = coordinatesY[i] - coordinatesY[i-1];
-double header = atan2(y_distance, x_distance);
-double angular_difference = header * 180 / M_PI;
-
-ROS_INFO("\nAngle: %f", angular_difference);
-}
     for(int i=0; i<sizeCoordA+2; i++){
   if (i == 0)
   {
@@ -145,62 +137,5 @@ ROS_INFO("\nAngle: %f", angular_difference);
     loop_rate.sleep();
   }
   }
-
-   /*
-       for(int i = 0; i < sizeCoordA; i++){
-      
-if(i == sizeCoordA-1){
-  ros::Rate loop_rate(2);
-  float randomX = startX;
-  float randomY = startY;
-  moveTurtle(randomX, randomY, teleport_client, srv);
-  loop_rate.sleep();
-}
-else {
-    int randomX = {randomNumber(10, 3)};
-    ros::Duration(1.0).sleep();
-    int randomY = {randomNumber(10, 3)};
-    moveTurtle(randomX, randomY, teleport_client, srv);
-    loop_rate.sleep();
-    }
-    */
-
-/* Exercise 2
-  pen_srv.request.off = true;
-  pen_client.call(pen_srv);
-  srv.request.x = startX;
-  srv.request.y = startY;
-  teleport_client.call(srv);
-  pen_srv.request.off = false;
-  pen_srv.request.width = 2;
-  pen_client.call(pen_srv);
-  loop_rate.sleep();
-
-  int sizeCoordA = 5;
-  int sizeCoordB = 2;
-  float coordinates[sizeCoordA][sizeCoordB] = {{startX+5, startY},{startX+5, startY+4},{startX+2, startY+4},{startX,startY+6},{startX, startY}};
-
-  for(int i=0; i<sizeCoordA; i++){
-    srv.request.x = coordinates[i][0];
-    srv.request.y = coordinates[i][1];
-    teleport_client.call(srv);
-    loop_rate.sleep();
-  }
-
-  */
-
-/* Exercise 1
-  int sizeCoordA = 5;
-  int sizeCoordB = 2;
-  float startX = 5.544445, startY = 5.544445;
-  float coordinates[sizeCoordA][sizeCoordB] = {{startX+3, startY},{startX+3, startY+4},{startX-3, startY+4},{startX-3,startY},{startX, startY}};
-
-  for(int i=0; i<sizeCoordA; i++){
-    srv.request.x = coordinates[i][0];
-    srv.request.y = coordinates[i][1];
-    teleport_client.call(srv);
-    loop_rate.sleep();
-  }
-*/
   return 0;
 }
